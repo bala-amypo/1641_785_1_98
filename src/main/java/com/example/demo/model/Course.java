02ea3123bb53c6bd494c85850542d63b86a00e97
bala-amypo <bala@amypo.in>
2025-12-18 15:37:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
+@Entity
 public class Course{
     @Id
     private Long id;
@@ -14,4 +15,42 @@ public class Course{
     private String instructor;
     private String category;
     private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getInstructor() {
+        return instructor;
+    }
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
