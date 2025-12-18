3862389badc71c80eddd7ad992e6fe33a44c5e56
bala-amypo <bala@amypo.in>
2025-12-18 15:22:18 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import java.sql.Data;
 public class User{
     private Long id;
     private String fullName;
@@ -8,4 +9,47 @@ public class User{
     private String role;
     private String preferredLearningStyle;
     private Date createdAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public void setPreferredLearningStyle(String preferredLearningStyle) {
+        this.preferredLearningStyle = preferredLearningStyle;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public String getPreferredLearningStyle() {
+        return preferredLearningStyle;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 }
