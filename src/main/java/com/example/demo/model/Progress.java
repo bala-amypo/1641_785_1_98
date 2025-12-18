7b92d47badf7c4a30244338ed4a2bfa01c9188b9
bala-amypo <bala@amypo.in>
2025-12-18 16:22:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Progress.java
+++ src/main/java/com/example/demo/model/Progress.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import java.sql.Data;
+import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -17,5 +18,48 @@ public class Progress{
     private Date lastAccessedAt;
     private BigDecimal score;
 
-    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public MicroLesson getMicroLesson() {
+        return microLesson;
+    }
+    public void setMicroLesson(MicroLesson microLesson) {
+        this.microLesson = microLesson;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public int getProgressPercent() {
+        return progressPercent;
+    }
+    public void setProgressPercent(int progressPercent) {
+        this.progressPercent = progressPercent;
+    }
+    public Date getLastAccessedAt() {
+        return lastAccessedAt;
+    }
+    public void setLastAccessedAt(Date lastAccessedAt) {
+        this.lastAccessedAt = lastAccessedAt;
+    }
+    public BigDecimal getScore() {
+        return score;
+    }
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+
 }
--- src/main/java/com/example/demo/model/Recommendation.java
+++ src/main/java/com/example/demo/model/Recommendation.java
@@ -1,11 +1,11 @@
 package com.example.demo.model;
 
 import java.sql.Data;
+import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
 @Entity
 public class Recommendation{
     @Id
