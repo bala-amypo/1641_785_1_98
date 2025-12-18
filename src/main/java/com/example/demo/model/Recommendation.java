5f2405737fb8ee5c21fe1892b2ecc4fdee76deb6
bala-amypo <bala@amypo.in>
2025-12-18 16:25:19 +0000

    User commit

--- src/main/java/com/example/demo/model/Recommendation.java
+++ src/main/java/com/example/demo/model/Recommendation.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
+
 @Entity
 public class Recommendation{
     @Id
@@ -15,4 +17,42 @@ public class Recommendation{
     private String recommendedLessonIds;
     private String basisSnapshot;
     private BigDecimal confidenceScore;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Date getGeneratedAt() {
+        return generatedAt;
+    }
+    public void setGeneratedAt(Date generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+    public String getRecommendedLessonIds() {
+        return recommendedLessonIds;
+    }
+    public void setRecommendedLessonIds(String recommendedLessonIds) {
+        this.recommendedLessonIds = recommendedLessonIds;
+    }
+    public String getBasisSnapshot() {
+        return basisSnapshot;
+    }
+    public void setBasisSnapshot(String basisSnapshot) {
+        this.basisSnapshot = basisSnapshot;
+    }
+    public BigDecimal getConfidenceScore() {
+        return confidenceScore;
+    }
+    public void setConfidenceScore(BigDecimal confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
+
 }
