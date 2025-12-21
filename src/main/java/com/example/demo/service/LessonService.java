9e4b78377ac43ff5c4667053d77d6b8f65f660b2
bala-amypo <bala@amypo.in>
2025-12-21 10:45:27 +0000

    User commit

--- src/main/java/com/example/demo/service/LessonService.java
+++ src/main/java/com/example/demo/service/LessonService.java
@@ -1,1 +1,14 @@
 package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.MicroLesson;
+
+public interface LessonService {
+    // MicroLesson addlesson (Long courseId,MicroLesson lesson)
+    MicroLesson post(MicroLesson ml);
+    MicroLesson getLesson(Long lessonId);
+    MicroLesson updateLesson(Long lessonId,MicroLesson lesson);
+    List<MicroLesson> getall();
+    String delemdata(Long id);
+} 
--- src/main/java/com/example/demo/service/impl/LessonServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LessonServiceImpl.java
@@ -1,3 +1,51 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.MicroLesson;
+import com.example.demo.repository.MicroLessonRepository;
+import com.example.demo.service.LessonService;
+
+@Service
+public class LessonServiceImpl implements LessonService {
+    @Autowired MicroLessonRepository mr;
+
+    @Override
+    public MicroLesson post(MicroLesson ml) {
+        return mr.save(ml);
+    }
+
+    @Override
+    public MicroLesson getLesson(Long lessonId) {
+        return mr.findById(lessonId).orElseThrow(()->new ResourceNotFoundException("Not found"));
+    }
+
+    @Override
+    public MicroLesson updateLesson(Long lessonId, MicroLesson lesson) {
+        if(mr.existsById(lessonId)){
+            lesson.setId(lessonId);
+            return mr.save(lesson);
+        }
+        throw new ResourceNotFoundException("Not found");
+    }
+
+    @Override
+    public List<MicroLesson> getall() {
+        return mr.findAll();
+    }
+
+    @Override
+    public String delemdata(Long id) {
+        if(mr.existsById(id)){
+            mr.deleteById(id);
+            return "Deleted successfully";
+        }
+        return "No data in that id";
+    }
+
+    
+}
