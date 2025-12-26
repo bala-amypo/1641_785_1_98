faee0a52df1fbf2e86b8d67eaa7cee838258583b
bala-amypo <bala@amypo.in>
2025-12-26 08:15:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Course;  // ← CHANGED
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.User;  // ← CHANGED from entity to model
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional; 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
