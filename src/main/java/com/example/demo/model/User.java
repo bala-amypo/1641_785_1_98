0500b3857beda5916154c6ba3c8d0ca000a00ff1
bala-amypo <bala@amypo.in>
2025-12-28 09:09:55 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,13 +1,49 @@
-package com.example.demo.model;
+// package com.example.demo.model;
 
+// import jakarta.persistence.*;
+// import lombok.*;
+
+// import java.time.LocalDateTime;
+
+// @Entity
+// @Table(name = "users")
+// @Data
+// @Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class User {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+    
+//     @Column(unique = true, nullable = false)
+//     private String email;
+    
+//     @Column(nullable = false)
+//     private String password;
+    
+//     private String role = "LEARNER";
+    
+//     @Column(name = "created_at")
+//     private LocalDateTime createdAt;
+    
+//     @PrePersist
+//     public void prePersist() {
+//         this.createdAt = LocalDateTime.now();
+//     }
+// }
+
+
+package com.example.demo.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
-
+import java.util.List;
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,20 +51,35 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique = true, nullable = false)
+
+    @Column(nullable = false, length = 100)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
     private String email;
-    
+
     @Column(nullable = false)
     private String password;
-    
-    private String role = "LEARNER";
-    
-    @Column(name = "created_at")
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(length = 50)
+    private String preferredLearningStyle;
+
     private LocalDateTime createdAt;
-    
+
+    @OneToMany(mappedBy = "instructor")
+    @JsonIgnore   
+    private List<Course> courses;
+
     @PrePersist
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
+        if (this.role == null) {
+            this.role = "LEARNER";
+        }
     }
 }
+
+
